9f563f2c15eccf701aa41604274103b8513ec5e1
yoonseop <alavusys@gmail.com>
2023-10-01 20:50:34 +0900

    Apply material-ui

--- src/componnents/Customer.js
+++ src/componnents/Customer.js
@@ -2,8 +2,6 @@ import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
-
-
 class  Customer extends React.Component {
     render() {
         return (
@@ -13,9 +11,8 @@ class  Customer extends React.Component {
                 <TableCell> {this.props.sex} </TableCell>
                 <TableCell> {this.props.job} </TableCell>
                 <TableCell> {this.props.name} </TableCell>
-                 <TableCell> {this.props.age} </TableCell>
+                <TableCell> {this.props.age} </TableCell>
             </TableRow>
-           
         )
     }
 }
